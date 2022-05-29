fefffbad8d3a1ccf10b7f2e0996fbda8fabd60df
Vosco Rash <r.vosco@gmail.com>
2022-05-29 23:56:19 +0000

    Added contact create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220529224406) do
+ActiveRecord::Schema.define(version: 20220529234949) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
